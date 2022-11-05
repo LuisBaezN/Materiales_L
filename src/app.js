9573b15361b66288d6ffc6543402b408598fe905
LuisBaezN <baez.201041081@gmail.com>
2022-11-04 22:53:46 -0600

    add: Materials page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,45 @@
 'use strict';
 
+// ////////////////////////////// Variables //////////////////////////////
+
 const espera = document.querySelector('.loader');
-const botonMenu = document.getElementsByClassName('menu')[0]
-const navSecMob = document.getElementsByClassName('nav-sec-mob')[0]
+const botonMenu = document.getElementsByClassName('menu')[0];
+const navSecMob = document.getElementsByClassName('nav-sec-mob')[0];
+let page = document.querySelectorAll('section')[0];
+
+// ////////////////////////////// Funciones //////////////////////////////
+
+function place(products){
+    console.log('> Placing...');
+
+    for (let i in products){
+        let sec = document.getElementsByClassName(i)[0];
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        li.innerHTML = '<h2>' + i + '</h2>';
+        ul.appendChild(li);
+        sec.appendChild(ul);
+        for (let j in products[i]){
+            li = document.createElement('li');
+            li.innerText = j;
+            ul.appendChild(li);
+        }
+        console.log(i);
+    }
+}
+
+// ////////////////////////////// Main //////////////////////////////
+
+console.log("> Running...");
+
+if (page.className === 'catalog'){
+    let prod = fetch('mater.json')
+    .then(datos => datos.json())
+    .then(datos => {
+        prod = datos;
+        place(prod);
+    })
+}
 
 window.addEventListener('load', () => {
     espera.classList.add('hide');
